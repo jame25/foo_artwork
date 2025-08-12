d709ecfdff2fb0ae7a102decf6eb5312dee47e9a
jame25 <jame25@users.noreply.github.com>
2025-08-12 13:07:15 +0100

    Update sdk_main.cpp

--- sdk_main.cpp
+++ sdk_main.cpp
@@ -281,7 +281,7 @@ BOOL APIENTRY DllMain(HMODULE hModule, DWORD ul_reason_for_call, LPVOID lpReserv
 #ifdef COLUMNS_UI_AVAILABLE
 DECLARE_COMPONENT_VERSION(
     "Artwork Display",
-    "1.5.10",
+    "1.5.11",
     "Cover artwork display component for foobar2000.\n"
     "Features:\n"
     "- Local artwork search (Cover.jpg, folder.jpg, etc.)\n"
@@ -298,7 +298,7 @@ DECLARE_COMPONENT_VERSION(
 #else
 DECLARE_COMPONENT_VERSION(
     "Artwork Display",
-    "1.5.10",
+    "1.5.11",
     "Cover artwork display component for foobar2000.\n"
     "Features:\n"
     "- Local artwork search (Cover.jpg, folder.jpg, etc.)\n"
