bf78a80bb51022694fb21c185e99f23e4b1bf9da
jame25 <jame25@users.noreply.github.com>
2025-08-26 10:07:09 +0100

    Update preferences.h

--- preferences.h
+++ preferences.h
@@ -1,5 +1,15 @@
 #pragma once
 #include "stdafx.h"
+#include <vector>
+
+// API types for priority ordering
+enum class ApiType {
+    iTunes = 0,
+    Deezer = 1,
+    LastFm = 2,
+    MusicBrainz = 3,
+    Discogs = 4
+};
 
 // External configuration variables
 extern cfg_bool cfg_enable_itunes;
@@ -9,3 +19,9 @@ extern cfg_string cfg_itunes_key;
 extern cfg_string cfg_discogs_key;
 extern cfg_string cfg_lastfm_key;
 extern cfg_uint cfg_cache_size;
+
+// Priority order configuration
+extern cfg_int cfg_search_order_1, cfg_search_order_2, cfg_search_order_3, cfg_search_order_4, cfg_search_order_5;
+
+// Function to get API search order based on user preferences
+std::vector<ApiType> get_api_search_order();
