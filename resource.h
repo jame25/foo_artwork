0fb235cf7dcf47b7dd852d4aaacc28ad3dd08d37
jame25 <jame25@users.noreply.github.com>
2025-12-30 23:34:34 +0000

    Update resource.h

--- resource.h
+++ resource.h
@@ -31,6 +31,7 @@
 #define IDC_STATIC_TIMEOUT              1027
 #define IDC_STATIC_RETRY                1028
 #define IDC_STATIC_SECONDS              1029
+#define IDC_ENABLE_DISK_CACHE           1030
 
 // Next default values for new objects
 // 
@@ -38,7 +39,7 @@
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_NEXT_RESOURCE_VALUE        103
 #define _APS_NEXT_COMMAND_VALUE         40001
-#define _APS_NEXT_CONTROL_VALUE         1030
+#define _APS_NEXT_CONTROL_VALUE         1031
 #define _APS_NEXT_SYMED_VALUE           101
 #endif
 #endif
