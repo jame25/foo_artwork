4984bf628298294fc952c1888126303b955e9af0
jame25 <jame25@users.noreply.github.com>
2026-02-16 02:23:56 +0000

    Update resource.h

--- resource.h
+++ resource.h
@@ -33,14 +33,15 @@
 #define IDC_STATIC_SECONDS              1029
 #define IDC_ENABLE_DISK_CACHE           1030
 #define IDC_BROWSE_CACHE_FOLDER         1031
+#define IDC_SKIP_LOCAL_ARTWORK          1032
 
 // Next default values for new objects
-// 
+//
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_NEXT_RESOURCE_VALUE        103
 #define _APS_NEXT_COMMAND_VALUE         40001
-#define _APS_NEXT_CONTROL_VALUE         1032
+#define _APS_NEXT_CONTROL_VALUE         1033
 #define _APS_NEXT_SYMED_VALUE           101
 #endif
 #endif
