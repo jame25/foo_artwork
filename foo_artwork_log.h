785a2ff66b046facc579542a4660036978481604
jame25 <jame25@users.noreply.github.com>
2026-08-01 03:32:36 +0100

    Update foo_artwork_log.h

--- foo_artwork_log.h
+++ foo_artwork_log.h
@@ -1,11 +1,25 @@
 #pragma once
 #include "stdafx.h"
 
-extern cfg_bool cfg_quiet_console;
+extern cfg_int cfg_console_logging_mode;
 
 namespace foo_artwork {
+    enum ConsoleLoggingMode {
+        MODE_QUIET = 0,
+        MODE_TRACK_INFO = 1,
+        MODE_DEBUG = 2
+    };
+
+    inline void log_track_info(const char* fmt, ...) {
+        if (cfg_console_logging_mode == MODE_QUIET) return;
+        va_list args;
+        va_start(args, fmt);
+        ::console::printfv(fmt, args);
+        va_end(args);
+    }
+
     inline void log_printf(const char* fmt, ...) {
-        if (cfg_quiet_console) return;
+        if (cfg_console_logging_mode != MODE_DEBUG) return;
         va_list args;
         va_start(args, fmt);
         ::console::printfv(fmt, args);
@@ -13,12 +27,12 @@ namespace foo_artwork {
     }
 
     inline void log_info(const char* msg) {
-        if (cfg_quiet_console) return;
+        if (cfg_console_logging_mode != MODE_DEBUG) return;
         ::console::info(msg);
     }
 
     inline void log_print(const char* msg) {
-        if (cfg_quiet_console) return;
+        if (cfg_console_logging_mode != MODE_DEBUG) return;
         ::console::print(msg);
     }
 }
