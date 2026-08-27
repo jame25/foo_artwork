b68f1d58cfc8bbdf481f5955371a5d949d775170
jame25 <jame25@users.noreply.github.com>
2026-08-28 00:22:21 +0100

    Update resource.h

--- resource.h
+++ resource.h
@@ -50,6 +50,7 @@
 #define IDC_STATIC_LOGOS_DESC2          1045
 #define IDC_STATIC_LOGOS_DESC3          1046
 #define IDC_STATIC_LOGOS_DESC4          1047
+#define IDC_DISABLE_INSTREAM_ARTWORK    1048
 
 // Next default values for new objects
 //
@@ -57,7 +58,7 @@
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_NEXT_RESOURCE_VALUE        104
 #define _APS_NEXT_COMMAND_VALUE         40001
-#define _APS_NEXT_CONTROL_VALUE         1048
+#define _APS_NEXT_CONTROL_VALUE         1049
 #define _APS_NEXT_SYMED_VALUE           101
 #endif
 #endif
